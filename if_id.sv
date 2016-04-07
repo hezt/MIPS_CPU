7e6ffe7934c7ca6656e4556e9a44c21a631c0d1e
Zitong He <hezt95@gmail.com>
2016-04-07 09:30:11 +0800

    Start pipline bypass

--- if_id.sv
+++ if_id.sv
@@ -14,26 +14,16 @@ module if_id(
     );
 	
 	always_ff @(posedge clk) begin
-		// if(nop_lock_id == 1'b0 && pc_bj == 1'b0) begin
 		if(nop_lock_id == 1'b0) begin
 			instruction_if_id <= instruction_if;
 			pc_if_id <= pc_if;
 		end
-		// else if(pc_bj == 1'b1) begin
-			// b or j clear
-			// instruction_if_id <= 0;
-			// pc_if_id <= 0;
-		// end
-		// else (nop_lock_id == 1'b1) begin
 		else begin
-			// insert bubble and keep constantly
 			instruction_if_id <= instruction_if_id;
 			pc_if_id <= pc_if_id;
 		end
 
 	end
-		
-
 endmodule
 
 `endif
